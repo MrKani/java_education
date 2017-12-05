5db5e4b62ad96ee7af3cf2b0ab06061f2ba42203
MrKani <korobchinskiyrs@sibvaleo.com>
2017-12-05 12:15:41 +0700

    changed

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyGirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyGirstProgram.java
@@ -1,9 +1,9 @@
 package ru.stqa.pft.sandbox;
 
 public class MyGirstProgram {
-	
-	public static void main(String[] args){
-		System.out.println("Hello, world!");
-	}
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 
 }
